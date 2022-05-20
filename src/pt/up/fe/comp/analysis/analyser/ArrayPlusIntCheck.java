bbf5c8989ff33182b4e60b12e052893e110686b7
sofiagermer <sofiagermer8@gmail.com>
2022-05-20 15:34:25 +0100

    check if variable is array working

--- src/pt/up/fe/comp/analysis/analyser/ArrayPlusIntCheck.java
+++ src/pt/up/fe/comp/analysis/analyser/ArrayPlusIntCheck.java
@@ -1,5 +1,6 @@
 package pt.up.fe.comp.analysis.analyser;
 
+import jdk.swing.interop.SwingInterOpUtils;
 import pt.up.fe.comp.analysis.SymbolTableBuilder;
 import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp.jmm.ast.PreorderJmmVisitor;
@@ -17,25 +18,33 @@ public class ArrayPlusIntCheck  extends PreorderJmmVisitor<Integer, Integer> {
     public ArrayPlusIntCheck(SymbolTableBuilder symbolTable, List<Report> reports) {
         this.reports = reports;
         this.symbolTable = symbolTable;
-        addVisit("Plus", this::visitPlus);
+        addVisit("Plus", this::visitArrayPlusInt);
 
         setDefaultVisit((node, oi) -> 0);
     }
-    public Integer visitPlus(JmmNode plusNode, Integer ret){
+    public Integer visitArrayPlusInt(JmmNode plusNode, Integer ret){
+        JmmNode left_node = plusNode.getJmmChild(0);
+        JmmNode right_node  = plusNode.getJmmChild(1);
         String method_name = plusNode.getAncestor("MethodDeclaration").get().getJmmChild(0).getJmmChild(1).get("name");
-        String left_node_name = plusNode.getJmmChild(0).get("name");
+        String left_node_name = left_node.get("name");
         String left_node_type = symbolTable.getVariableType(method_name,left_node_name).getName();
-        String right_node_name = plusNode.getJmmChild(1).get("name");
+        String right_node_name = right_node.get("name");
         String right_node_type = symbolTable.getVariableType(method_name,right_node_name).getName();
-        System.out.println("Type left: " + left_node_type);
-        System.out.println("Name left: " + left_node_name);
-        System.out.println("Type right: " + left_node_type);
+        System.out.println("method name: " + method_name);
+        System.out.println("left node kind : " + left_node.getKind());
+        System.out.println("right node kind : " + right_node.getKind());
+        System.out.println("left node type : " + left_node_type);
+        System.out.println("right node type : " + right_node_type);
+        System.out.println("left node children : " + left_node.getAttributes());
+        System.out.println("right node children : " + right_node.getAttributes());
+        System.out.println("node left is array: " + symbolTable.isArray(method_name, left_node_name));
+        System.out.println("node right is array: " + symbolTable.isArray(method_name, right_node_name));
 
-
+        /*
         if (left_node_type.equals("boolean") && right_node_type.equals("int")){
             reports.add(Report.newError(Stage.SEMANTIC, -1, -1, "\"" + left_node_type + "\" invalid type: expecting an boolean.", null));
         }
-        /*else if (UtilsAnalyser.getVariableType(rightNode, symbolTable).equals("object") && leftNode.getKind().equals("Number")){
+        else if (UtilsAnalyser.getVariableType(rightNode, symbolTable).equals("object") && leftNode.getKind().equals("Number")){
             reports.add(Report.newError(Stage.SEMANTIC, -1, -1, "\"" + leftNode + "\" invalid type: expecting an boolean.", null));
         }
         else if (UtilsAnalyser.getVariableType(leftNode, symbolTable).equals("int") && UtilsAnalyser.getVariableType(rightNode, symbolTable).equals("object")){
