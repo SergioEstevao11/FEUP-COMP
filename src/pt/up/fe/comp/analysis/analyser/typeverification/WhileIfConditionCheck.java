3c329686543a8904742a1a98055463f6f02f602a
JoaoMIEIC <afonso2345@gmail.com>
2022-06-15 23:20:07 +0100

    feat: launcher update

--- src/pt/up/fe/comp/analysis/analyser/typeverification/WhileIfConditionCheck.java
+++ src/pt/up/fe/comp/analysis/analyser/typeverification/WhileIfConditionCheck.java
@@ -22,7 +22,6 @@ public class WhileIfConditionCheck extends PreorderJmmVisitor<Integer, Integer> 
     }
     public Integer visitIfWhileCondition(JmmNode whileIfStatementNode, Integer ret) {
         String method_name = null;
-
         if( whileIfStatementNode.getAncestor("MethodDeclaration").get().getJmmChild(0).getKind().equals("MainMethodHeader")) method_name = "main";
         else method_name = whileIfStatementNode.getAncestor("MethodDeclaration").get().getJmmChild(0).getJmmChild(1).get("name");
 
